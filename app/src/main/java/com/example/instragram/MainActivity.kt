2e9074360bdb99c7741aad9687ff318a5dc3a263
saadCIT <saadibnahabib0000@gmail.com>
2024-10-07 12:56:36 +0600

    Instagram log in page design

---
+++ app/src/main/java/com/example/instragram/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.instragram
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.instragram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+
+            Toast.makeText(this@MainActivity,"Login Successful",Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+}
